2b90daf4a3f2101cdbfde8d1c71b5c4a06ad6f1b
priiiya3 <priyasingh3709@gmail.com>
2024-06-14 23:59:41 +0530

    day 20

--- Daily Question/Minimum Increment to Make Array Unique.py
+++ Daily Question/Minimum Increment to Make Array Unique.py
@@ -27,3 +27,16 @@ Constraints:
 1 <= nums.length <= 105
 0 <= nums[i] <= 105
 """
+
+class Solution:
+    def minIncrementForUnique(self, nums: List[int]) -> int:
+        
+        nums.sort()
+        cnt = 0
+        for i in range(1, len(nums)):
+            if nums[i] <= nums[i-1]:
+                diff = ((nums[i-1]+1) - nums[i])
+                nums[i] += diff
+                cnt += diff
+
+        return cnt
