1a178286ff5a5fc7913864173ce6ea1a2f53c535
priiiya3 <priyasingh3709@gmail.com>
2024-07-01 21:18:34 +0530

    day 34

--- Daily Question/Three Consecutive Odds.py
+++ Daily Question/Three Consecutive Odds.py
@@ -2,3 +2,32 @@
 
 # Leetcode Problem 1550: Three Consecutive Odds
 
+# Problem Statement
+"""
+Given an integer array arr, return true if there are three consecutive odd numbers in the array. Otherwise, return false.
+ 
+
+Example 1:
+
+Input: arr = [2,6,4,1]
+Output: false
+Explanation: There are no three consecutive odds.
+Example 2:
+
+Input: arr = [1,2,34,3,4,5,7,23,12]
+Output: true
+Explanation: [5,7,23] are three consecutive odds.
+ 
+
+Constraints:
+
+1 <= arr.length <= 1000
+1 <= arr[i] <= 1000
+"""
+
+# Approach:
+"""
+Since this is a Easy level problem and contraints are not too large. 
+I am gonna simply loop through the array and find the i, i+1 and i+2 element who are odd.
+Time complexity is O(N) and Space Complexity is O(1).
+"""
