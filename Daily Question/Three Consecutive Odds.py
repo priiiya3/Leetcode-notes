01f755d62f850d9eeeca53debbdc758b3064353a
priiiya3 <priyasingh3709@gmail.com>
2024-07-01 21:20:47 +0530

    Day 34

--- Daily Question/Three Consecutive Odds.py
+++ Daily Question/Three Consecutive Odds.py
@@ -31,3 +31,27 @@ Since this is a Easy level problem and contraints are not too large.
 I am gonna simply loop through the array and find the i, i+1 and i+2 element who are odd.
 Time complexity is O(N) and Space Complexity is O(1).
 """
+
+# Solution:
+from typing import List
+
+
+class Solution:
+    def threeConsecutiveOdds(self, arr: List[int]) -> bool:
+        
+        for i in range(len(arr)-2):
+            if arr[i]%2 and arr[i+1]%2 and arr[i+2] % 2:
+                return True
+            
+        return False
+
+# Dirver's Code
+Result = Solution()
+
+# Test Case 1:
+testcase1 = Result.threeConsecutiveOdds(arr = [2, 6, 4, 1])
+print(testcase1) # expected: False
+
+# Test Case 2:
+testcase2 = Result.threeConsecutiveOdds(arr = [1,2,34,3,4,5,7,23,12])
+print(testcase2) # expected: True
