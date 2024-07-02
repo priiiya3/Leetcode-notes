489192a0847bd3cd7418e5039c7cbbdaef0c3045
priiiya3 <priyasingh3709@gmail.com>
2024-07-02 22:33:32 +0530

    day 35

--- Daily Question/Intersection of Two Arrays II.py
+++ Daily Question/Intersection of Two Arrays II.py
@@ -38,3 +38,31 @@ Mujhe easiest approach ye laga ki main pahle ek hashmap me first array k har ele
 Phir second array me traverse krke check karu ki second array ka elment i, kya mere hashmap me hai, agar hai to 
 add it into my res array
 """
+
+# Solution:
+from typing import List
+
+
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        if len(nums2) > len(nums1): return self.intersect(nums2, nums1)
+        
+        cnt = Counter(nums1)
+        res = []
+        for i in range(len(nums2)):
+            if cnt[nums2[i]] > 0:
+                res.append(nums2[i])
+                cnt[nums2[i]] -= 1
+                
+        return res
+
+# Driver's Code:
+Result = Solution()
+
+# Test Case 1:
+testcase1 = Result.intersect(nums1 = [1,2,2,1], nums2 = [2,2])
+print(testcase1) # expected: [2, 2]
+
+# Test Case 2:
+testcase2 = Result.intersect(nums1 = [4,9,5], nums2 = [9,4,9,8,4])
+print(testcase2) # expected: [4, 9]
